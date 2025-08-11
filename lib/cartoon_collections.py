c508f5ec852661ddf94895a40ac339781f3a840e
Mireau-Jules <saintlouismireaujules@gmail.com>
2025-08-11 11:15:38 -0400

    Complete Lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarf_names): 
+    i = 0
+    while i < len(dwarf_names):
+        dwarf_name = dwarf_names[i]
+        print(f"{i + 1}. {dwarf_name}")
+        i += 1
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f"{planeteer_call.capitalize()}!" for planeteer_call in planeteer_calls ]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(assorted_words):
+    return any(len(assorted_word) > 4 for assorted_word in assorted_words)
+
+def find_the_cheese(foods):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in foods:
+        if item in cheeses:
+            return item
+    return None
+    
